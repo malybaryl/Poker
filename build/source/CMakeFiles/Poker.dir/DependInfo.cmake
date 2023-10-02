7266f6924a5175790fff490267298689083bed58
Kacper Baryłowicz <112568080+malybaryl@users.noreply.github.com>
2023-10-02 12:02:57 +0200

    Add files via upload

---
+++ build/source/CMakeFiles/Poker.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kacper/Desktop/Programming/cpp/Code/Poker/source/checkCards.cpp" "source/CMakeFiles/Poker.dir/checkCards.cpp.o" "gcc" "source/CMakeFiles/Poker.dir/checkCards.cpp.o.d"
+  "/home/kacper/Desktop/Programming/cpp/Code/Poker/source/drawCards.cpp" "source/CMakeFiles/Poker.dir/drawCards.cpp.o" "gcc" "source/CMakeFiles/Poker.dir/drawCards.cpp.o.d"
+  "/home/kacper/Desktop/Programming/cpp/Code/Poker/source/main.cpp" "source/CMakeFiles/Poker.dir/main.cpp.o" "gcc" "source/CMakeFiles/Poker.dir/main.cpp.o.d"
+  "/home/kacper/Desktop/Programming/cpp/Code/Poker/source/transformCardsName.cpp" "source/CMakeFiles/Poker.dir/transformCardsName.cpp.o" "gcc" "source/CMakeFiles/Poker.dir/transformCardsName.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/source/CMakeFiles/Poker.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Poker.dir/checkCards.cpp.o"
+  "CMakeFiles/Poker.dir/checkCards.cpp.o.d"
+  "CMakeFiles/Poker.dir/drawCards.cpp.o"
+  "CMakeFiles/Poker.dir/drawCards.cpp.o.d"
+  "CMakeFiles/Poker.dir/main.cpp.o"
+  "CMakeFiles/Poker.dir/main.cpp.o.d"
+  "CMakeFiles/Poker.dir/transformCardsName.cpp.o"
+  "CMakeFiles/Poker.dir/transformCardsName.cpp.o.d"
+  "Poker"
+  "Poker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Poker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
